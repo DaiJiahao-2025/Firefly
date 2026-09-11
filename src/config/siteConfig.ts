9d087763ecfa6ed8a9dd974959d9aa7c1c5b484d
DaiJiahao-2025 <djh167331@gmail.com>
2026-09-11 14:00:31 +0800

    更新页面标题

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -42,17 +42,17 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "haolove",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "blog",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://haolove.top",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"这里是hao的博客，记录技术和生活以及自己的一些思考，在互联网上留下足迹。",
 
 	// 站点关键词
 	keywords: [
@@ -117,7 +117,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "Hao Blog",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
